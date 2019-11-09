a582f55418e85e6844fc493720cb00f5a322b864
PekkaPek <pekpekk@laurea.fi>
2019-11-09 13:05:28 +0200

    Initial commit

---
+++ currency-adder.ps1
@@ -0,0 +1,8 @@
+﻿Param(
+    [Parameter(Position=1)]
+    [string]$resultAs
+)
+
+[string]$apiCall = "https://api.exchangeratesapi.io/latest" + "?base=" + $resultAs
+
+Invoke-RestMethod -Uri $apiCall
