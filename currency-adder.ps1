586de34df823efe29b187235b4c9d6e4a6e43e71
PekkaPek <pekpekk@laurea.fi>
2019-11-09 13:43:42 +0200

    Extract currency information  from script parameters

--- currency-adder.ps1
+++ currency-adder.ps1
@@ -1,8 +1,12 @@
-﻿Param(
-    [Parameter(Position=1)]
-    [string]$resultAs
-)
+﻿$resultAs = $args[0]
+$amount = @(0) * $args.Length
+$currency = @(0) * $args.Length
+
+for ($i=1; $i -le $args.Length; $i++) {
+    $amount[$i-1] = $args[$i] -replace "[^0-9]" , ''
+    $currency[$i-1] = $args[$i] -replace '[^a-zA-Z-]',''
+}
 
 [string]$apiCall = "https://api.exchangeratesapi.io/latest" + "?base=" + $resultAs
-
+    
 Invoke-RestMethod -Uri $apiCall
