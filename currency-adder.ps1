cc5813085c02cc7c78f29fd7972a9204b92b14b1
PekkaPek <pekpekk@laurea.fi>
2019-11-10 11:59:20 +0200

    Remove extra array item

--- currency-adder.ps1
+++ currency-adder.ps1
@@ -1,6 +1,6 @@
 ﻿$resultAs = $args[0]
-$amount = @(0) * $args.Length
-$currency = @(0) * $args.Length
+$amount = @(0) * ($args.Length - 1)
+$currency = @(0) * ($args.Length  - 1)
 
 for ($i=1; $i -lt $args.Length; $i++) {
     $amount[$i-1] = $args[$i] -replace "[^0-9]" , ''
@@ -12,7 +12,7 @@ for ($i=1; $i -lt $args.Length; $i++) {
 $exhangeRates = Invoke-RestMethod -Uri $apiCall
 
 $totalAmount = 0
-for ($i=0; $i -lt ($amount.Length - 1); $i++) {
+for ($i=0; $i -lt $amount.Length; $i++) {
     if( $resultAs -eq $currency[$i]) {
         $totalAmount += $amount[$i]
     } else {
